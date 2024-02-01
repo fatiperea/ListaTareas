da656eeeae24ea8d58f1a2c85d5ebcd5911a6fc5
fatiperea <fatimaperea3pppp0@gmail.com>
2024-02-01 14:43:32 -0300

    comp footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import FormTareas from './components/FormTareas';
+import Footer from './components/Footer';
 import { Container } from 'react-bootstrap';
 
 function App() {
 
   return (
+    <> 
     <Container className='my-4'>
       <h1 className='text-center display-3'>LISTA DE TAREAS</h1>
       <FormTareas></FormTareas>
     </Container>
+    <Footer></Footer>
+    </>
   )
 }
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className='bg-dark text-light text-center py-3'>
+            <p>&copy; Todos los derechos reservados</p>
+        </footer>
+    );
+};
+
+export default Footer;
