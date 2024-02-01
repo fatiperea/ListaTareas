a62992f7dfec8ab7855ccd9ecb0fd7e4566893f8
fatiperea <fatimaperea3pppp0@gmail.com>
2024-02-01 19:57:12 -0300

    state tareas

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './App.css'
 import FormTareas from './components/FormTareas';
 import Footer from './components/Footer';
@@ -8,7 +9,7 @@ function App() {
 
   return (
     <> 
-    <Container className='my-4'>
+    <Container className='my-4 mainPage'>
       <h1 className='text-center display-3'>LISTA DE TAREAS</h1>
       <FormTareas></FormTareas>
     </Container>
--- src/components/FormTareas.jsx
+++ src/components/FormTareas.jsx
@@ -1,16 +1,31 @@
-import { Button, Form } from 'react-bootstrap';
-import ListaTareas from './ListaTareas';
+import { Button, Form } from "react-bootstrap";
+import { useState } from "react";
+import ListaTareas from "./ListaTareas";
 
 const FormTareas = () => {
+  const [tarea, setTarea] = useState("");
+  const [tareas, setTareas]= useState([]);
+
   return (
     <section>
       <Form>
-        <Form.Group className="mb-3 d-flex" controlId="exampleForm.ControlInput1">
+        <Form.Group
+          className="mb-3 d-flex"
+          controlId="exampleForm.ControlInput1"
+        >
           {/*}<Form.Label>Ingresar tareas:</Form.Label>{*/}
-          <Form.Control type="text" placeholder="Ej: tarea 1" minLength={3} maxLength={50} />
-          <Button variant='info' className='mx-2' type='submit'>Agregar tarea</Button>
+          <Form.Control
+            type="text"
+            placeholder="Ej: tarea 1"
+            minLength={3}
+            maxLength={50}
+            onChange={(e)=>{setTarea(e.target.value)}}
+            value={tarea}
+          />
+          <Button variant="info" className="mx-2" type="submit">
+            Agregar tarea
+          </Button>
         </Form.Group>
-        
       </Form>
       <ListaTareas></ListaTareas>
     </section>
