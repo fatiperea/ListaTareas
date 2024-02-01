c05fc6e20d0914204422b2f84894bfb1f0daa504
fatiperea <fatimaperea3pppp0@gmail.com>
2024-02-01 14:12:55 -0300

    formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
+import FormTareas from './components/FormTareas';
+import { Container } from 'react-bootstrap';
 
 function App() {
-  
+
   return (
-    <>
-    <h1>HOLA</h1>
-      </>
+    <Container className='my-4'>
+      <h1 className='text-center display-3'>LISTA DE TAREAS</h1>
+      <FormTareas></FormTareas>
+    </Container>
   )
 }
 
