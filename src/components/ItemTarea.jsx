026d7cd6ab09c3242e883d03a35209361ae33751
fatiperea <fatimaperea3pppp0@gmail.com>
2024-02-01 21:28:32 -0300

    agrego btn borrar

--- src/components/ItemTarea.jsx
+++ src/components/ItemTarea.jsx
@@ -1,9 +1,11 @@
 import { ListGroup } from "react-bootstrap";
 
-const ItemTarea = ({nombreTarea}) => {
-    return (
-        <ListGroup.Item>{nombreTarea}</ListGroup.Item>
-    );
+const ItemTarea = ({ nombreTarea }) => {
+  return (
+    <ListGroup.Item className="d-flex justify-content-between">
+      {nombreTarea} <Button variant="danger">Borrar</Button>
+    </ListGroup.Item>
+  );
 };
 
 export default ItemTarea;
