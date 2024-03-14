90ae829144b8bd0c4c6da01ebc310c845dad53c5
fatiperea <fatimaperea3pppp0@gmail.com>
2024-03-14 01:17:19 -0300

    commit para poder hacer merge de dev

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-import './App.css'
-import FormTareas from './components/FormTareas';
-import Footer from './components/Footer';
-import { Container } from 'react-bootstrap';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import FormTareas from "./components/FormTareas";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
 
 function App() {
-
   return (
-    <> 
-    <Container className='my-4 mainPage'>
-      <h1 className='text-center display-3'>LISTA DE TAREAS</h1>
-      <FormTareas></FormTareas>
-    </Container>
-    <Footer></Footer>
+    <>
+      <Container className="my-4 mainPage">
+        <h1 className="text-center display-3">LISTA DE TAREAS</h1>
+        <FormTareas></FormTareas>
+      </Container>
+      <Footer></Footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
